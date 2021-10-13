f259b9a274dc1598602a1fb732ba8927e446fa39
panoramine <camila.c.a.salles@gmail.com>
2021-10-12 19:28:13 -0700

    add 4_queries

---
+++ 4_queries/11_name_of_teachers_that_assisted.sql
@@ -0,0 +1,8 @@
+SELECT teachers.name, cohorts.name AS cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY teachers.name, cohorts.name
+HAVING cohorts.name = 'JUL02'
+ORDER BY teachers.name;
---
+++ 4_queries/5_cohort_with_longest_assistances.sql
@@ -0,0 +1,9 @@
+SELECT cohorts.name AS name, AVG(completed_at - started_at) AS average_assistance_time
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING AVG(completed_at - started_at) > ALL 
+(SELECT AVG(completed_at - started_at) FROM assistance_requests)
+ORDER BY average_assistance_time DESC
+LIMIT 1;
